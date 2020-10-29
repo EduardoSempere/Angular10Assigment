670b37bed838806c154e462c58ffee0cb3aa1f63
Edaurdo Sempere <esempere@sistemastg.com>
2020-10-28 23:17:03 -0700

    primera tare

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { StarComponent } from './star/star.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+    StarComponent
   ],
   imports: [
     BrowserModule,
